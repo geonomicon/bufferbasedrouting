e252de7e5a0f9b088d16b784a03b2abef62faea8
rajat <rajat@tyrannodrake>
2016-07-24 00:46:02 +0530

    awesome

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,25 @@ exports.index = function (req, res) {
 
 exports.bufferBasedRouting = function (req, res) {
     var pickers = [];
-    req.params.users = pickers;
-    pickupAddress = req.params.pickupAddress;
+    pickers = req.body.users;
+    pickupAddress = req.body.pickupAddress;
     request('http://api.opencagedata.com/geocode/v1/json?q=' + pickupAddress + '&key=93d639c2f2e101a955c9dd2ec8704fca', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             toSend = JSON.parse(body);
             pickupLat = toSend.results[0].geometry.lat;
             pickupLng = toSend.results[0].geometry.lng;
+            pickers.sort(function (a, w, e, s, o, me) {
+                p = Math.pow((pickupLat - a.latitude), 2);
+                q = Math.pow((pickupLng - a.longitude), 2);
+                r = Math.pow((pickupLat - w.latitude), 2);
+                s = Math.pow((pickupLng - w.longitude), 2);
+                t = Math.pow((p + q), 1 / 2);
+                u = Math.pow((r + s), 1 / 2);
+                return t-u;
+            });
+            res.json(pickers);
+        } else {
+            res.json({'error':'Could not Geocode Address'});
         }
     });
-    pickers.sort(function (a, b) {
-        return Math.pow((Math.pow(pickupLat - a.latitude), 2) - Math.pow((pickupLng - a.longitude), 2), 1 / 2) - Math.pow((Math.pow(pickupLat - b.latitude), 2) - Math.pow((pickupLng - b.longitude), 2), 1 / 2);
-    });
-    res.json(pickers);
 };
