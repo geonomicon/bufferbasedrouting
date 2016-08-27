124791684d96fa050fa78919dfa336f765c4428b
rajat <rajat@tyrannodrake>
2016-08-28 05:01:59 +0530

    Current Picker Index added

--- routes/index.js
+++ routes/index.js
@@ -52,53 +52,55 @@ exports.testPost = function(req, res) {
 };
 
 exports.bufferBasedRouting = function(io) {
-    return function(req, res) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        var pickers = [];
-        if (!req.body.availabeExecutives) {
-            res.json({
-                'error': 'Invalid Response, try again',
-                'details': 'Argument availabeExecutives not found or response structure not valid'
-            });
-        };
-        if (!req.body.pickupAddress) {
-            res.json({
-                'error': 'Invalid Response, try again',
-                'details': 'Argument pickupAddress not found or response structure not valid'
-            });
-        };
-        pickers = req.body.availabeExecutives;
-        pickupAddress = req.body.pickupAddress;
-        request('http://api.opencagedata.com/geocode/v1/json?q=' + pickupAddress + '&key=93d639c2f2e101a955c9dd2ec8704fca', function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                toSend = JSON.parse(body);
-                pickupLat = toSend.results[0].geometry.lat;
-                pickupLng = toSend.results[0].geometry.lng;
-
-                pickers.sort(function(a, w, e, s, o, me) {
-                    p = Math.pow((pickupLat - a.latitude), 2);
-                    q = Math.pow((pickupLng - a.longitude), 2);
-                    r = Math.pow((pickupLat - w.latitude), 2);
-                    s = Math.pow((pickupLng - w.longitude), 2);
-                    t = Math.pow((p + q), 1 / 2);
-                    u = Math.pow((r + s), 1 / 2);
-                    return t - u;
-                });
-                var sendUsers = {
-                    orignalBody: req.body,
-                    pickers,
-                    pickedBy: null,
-                    currentPicker: req.body.availabeExecutives[0].userid
-                };
-                var messageListRef = Firebase.database().ref('items');
-                messageListRef.push(sendUsers);
-                res.json(pickers);
-            } else {
+        return function(req, res) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            var pickers = [];
+            if (!req.body.availabeExecutives) {
                 res.json({
-                    'error': 'Could not Geocode Address'
+                    'error': 'Invalid Response, try again',
+                    'details': 'Argument availabeExecutives not found or response structure not valid'
                 });
+            };
+            if (!req.body.pickupAddress) {
+                res.json({
+                    'error': 'Invalid Response, try again',
+                    'details': 'Argument pickupAddress not found or response structure not valid'
+                });
+            };
+            pickers = req.body.availabeExecutives;
+            pickupAddress = req.body.pickupAddress;
+            request('http://api.opencagedata.com/geocode/v1/json?q=' + pickupAddress + '&key=93d639c2f2e101a955c9dd2ec8704fca', function(error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        toSend = JSON.parse(body);
+                        pickupLat = toSend.results[0].geometry.lat;
+                        pickupLng = toSend.results[0].geometry.lng;
+
+                        pickers.sort(function(a, w, e, s, o, me) {
+                            p = Math.pow((pickupLat - a.latitude), 2);
+                            q = Math.pow((pickupLng - a.longitude), 2);
+                            r = Math.pow((pickupLat - w.latitude), 2);
+                            s = Math.pow((pickupLng - w.longitude), 2);
+                            t = Math.pow((p + q), 1 / 2);
+                            u = Math.pow((r + s), 1 / 2);
+                            return t - u;
+                        });
+                            var sendUsers = {
+                                orignalBody: req.body,
+                                pickers,
+                                pickedBy: null,
+                                currentPickerIndex: 0,
+                                currentPicker: req.body.availabeExecutives[0].userid
+                            };
+
+                            var messageListRef = Firebase.database().ref('items');
+                            messageListRef.push(sendUsers);
+                            res.json(pickers);
+                        } else {
+                            res.json({
+                                'error': 'Could not Geocode Address'
+                            });
+                        }
+                    });
             }
-        });
-    }
-};
+        };
