16dc7c50b741934181d29c272a59b60363ebcfa7
tyrannodrake <tyrannodrake@gmail.com>
2016-10-13 18:23:35 +0530

    geocoding no results

--- routes/index.js
+++ routes/index.js
@@ -52,78 +52,80 @@ exports.testPost = function(req, res) {
 };
 
 exports.bufferBasedRouting = function(io) {
-        return function(req, res) {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            var pickers = [];
-            if (!req.body.availabeExecutives) {
-                res.json({
-                    'error': 'Invalid Response, try again',
-                    'details': 'Argument availabeExecutives not found or response structure not valid'
+    return function(req, res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        var pickers = [];
+        if (!req.body.availabeExecutives) {
+            res.json({
+                'error': 'Invalid Response, try again',
+                'details': 'Argument availabeExecutives not found or response structure not valid'
+            });
+            return;
+        };
+        if (req.body.availabeExecutives.length == 0) {
+            res.json({
+                'error': 'Invalid Response, try again',
+                'details': 'Argument availabeExecutives has no Users'
+            });
+            return;
+        };
+        if (!req.body.pickupAddress) {
+            res.json({
+                'error': 'Invalid Response, try again',
+                'details': 'Argument pickupAddress not found or response structure not valid'
+            });
+            return;
+        };
+        pickers = req.body.availabeExecutives;
+        pickupAddress = req.body.pickupAddress;
+        request('http://api.opencagedata.com/geocode/v1/json?q=' + pickupAddress + '&key=93d639c2f2e101a955c9dd2ec8704fca', function(error, response, body) {
+            if (!error && response.statusCode == 200 && JSON.parse(body).results.length > 0) {
+                toSend = JSON.parse(body);
+                pickupLat = toSend.results[0].geometry.lat;
+                pickupLng = toSend.results[0].geometry.lng;
+
+                pickers.sort(function(a, w, e, s, o, me) {
+                    p = Math.pow((pickupLat - a.latitude), 2);
+                    q = Math.pow((pickupLng - a.longitude), 2);
+                    r = Math.pow((pickupLat - w.latitude), 2);
+                    s = Math.pow((pickupLng - w.longitude), 2);
+                    t = Math.pow((p + q), 1 / 2);
+                    u = Math.pow((r + s), 1 / 2);
+                    return t - u;
                 });
-                return;
-            };
-            if (req.body.availabeExecutives.length==0) {
-                res.json({
-                    'error': 'Invalid Response, try again',
-                    'details': 'Argument availabeExecutives has no Users'
-                });
-                return;
-            };
-            if (!req.body.pickupAddress) {
-                res.json({
-                    'error': 'Invalid Response, try again',
-                    'details': 'Argument pickupAddress not found or response structure not valid'
-                });
-                return;
-            };
-            pickers = req.body.availabeExecutives;
-            pickupAddress = req.body.pickupAddress;
-            request('http://api.opencagedata.com/geocode/v1/json?q=' + pickupAddress + '&key=93d639c2f2e101a955c9dd2ec8704fca', function(error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        toSend = JSON.parse(body);
-                        pickupLat = toSend.results[0].geometry.lat;
-                        pickupLng = toSend.results[0].geometry.lng;
 
-                        pickers.sort(function(a, w, e, s, o, me) {
-                            p = Math.pow((pickupLat - a.latitude), 2);
-                            q = Math.pow((pickupLng - a.longitude), 2);
-                            r = Math.pow((pickupLat - w.latitude), 2);
-                            s = Math.pow((pickupLng - w.longitude), 2);
-                            t = Math.pow((p + q), 1 / 2);
-                            u = Math.pow((r + s), 1 / 2);
-                            return t - u;
-                        });
+                var sendUsers = {
+                    orignalBody: req.body,
+                    pickers,
+                    pickedBy: null,
+                    currentPickerIndex: 0,
+                    currentPicker: req.body.availabeExecutives[0].userid
+                };
 
-                        var sendUsers = {
-                            orignalBody: req.body,
-                            pickers,
-                            pickedBy: null,
-                            currentPickerIndex: 0,
-                            currentPicker: req.body.availabeExecutives[0].userid
-                        };
-                        
-                        request.post({
-                                url: 'https://dharasabha.firebaseio.com/.json',
-                                json: sendUsers},
-                                function(err, httpResponse, body) {
-                                    if (!error && response.statusCode == 200) {
-                                        console.log('Successfully sent to fb server');
-                                    } else {
-                                        console.log('Unable to save data to firebase server');
-                                    }
-                                });
-
-                            var messageListRef = Firebase.database().ref('items'); messageListRef.push(sendUsers, function() {
-                                Firebase.database().goOffline();
-                            }); res.json(pickers);
-
-                        }
-                        else {
-                            res.json({
-                                'error': 'Could not Geocode Address'
-                            });
+                request.post({
+                        url: 'https://dharasabha.firebaseio.com/.json',
+                        json: sendUsers
+                    },
+                    function(err, httpResponse, body) {
+                        if (!error && response.statusCode == 200) {
+                            console.log('Successfully sent to fb server');
+                        } else {
+                            console.log('Unable to save data to firebase server');
                         }
                     });
+
+                var messageListRef = Firebase.database().ref('items');
+                messageListRef.push(sendUsers, function() {
+                    Firebase.database().goOffline();
+                });
+                res.json(pickers);
+
+            } else {
+                res.json({
+                    'error': 'Could not Geocode Address'
+                });
             }
-        };
+        });
+    }
+};
